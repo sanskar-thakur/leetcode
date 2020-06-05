bb964d08170406a23c6f1737781a5176fed85268
Sanskaar Thakur <64678688+sanskaar-thakur@users.noreply.github.com>
2020-06-05 20:20:50 +0530

    June challenge - Problem 4

---
+++ June_P4_ReverseString.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        if(s.size() == 0) return;
+        
+        char temp;                
+        for(int i = 0, j = s.size() - 1;i < j; i++, j--){            
+            temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;            
+        }        
+    }
+};
